e57a6100e6b9223b8cf633295b7cf591be5297fa
Kamil Mróz <kamil.2003.kmz@gmail.com>
2024-04-06 23:37:32 +0200

    last version

--- Server/cmake-build-debug/cmake_install.cmake
+++ Server/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kamilmroz/CLionProjects/RPicoWConnection
+# Install script for directory: /Users/kamilmroz/Desktop/hacknarok/Server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/kamilmroz/CLionProjects/RPicoWConnection/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/kamilmroz/Desktop/hacknarok/Server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
