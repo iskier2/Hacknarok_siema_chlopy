05848e8a48c33212dbded139441558b3b4d2d96a
Kamil Mróz <kamil.2003.kmz@gmail.com>
2024-04-07 02:05:20 +0200

    wrzucenie plikow raspberry do folderu (rozdzielenie plikow serwerowych i rasp)

--- Server/server.js
+++ Server/server.js
@@ -9,25 +9,6 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('cmake-build-debug/SKW.db');
 // Create a table
 
-
-// const server = http.createServer((req, res) => {
-//     // Set content type to HTML
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//
-//     // Read the HTML file
-//     fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-//         if (err) {
-//             res.writeHead(404);
-//             res.write('File not found!');
-//         } else {
-//             // Send the HTML content to the client
-//             res.write(data);
-//         }
-//         res.end();
-//     });
-// });
-
-
 const server = http.createServer((req, res) => {
     // Handle requests to the root path
     if (req.url === '/') {
